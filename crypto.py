bc13f1e53487efbe3af471ed531e8705f0e7cf50
Ben Herzog <benherzog11235@gmail.com>
2015-05-13 19:02:14 +0300

    adding first versions of source files

---
+++ crypto.py
@@ -0,0 +1,190 @@
+#! /usr/bin/python
+from __future__ import division
+from math import log
+import string
+import itertools
+import operator
+import random
+
+MIN_PROB = 0.00001
+
+surprise = lambda p: log(1/MIN_PROB,2) if p==0 else log(1/p,2)
+class Distribution:
+	def __init__(self,probs):
+		self.probs=probs
+	def __iter__(self):
+		for c in self.probs: yield c
+	def __getitem__(self,index):
+		try: return self.probs[index]
+		except KeyError: return 0
+	def __setitem__(self,index,value):
+		self.probs[index] = value
+	def __repr__(self):
+		return str(self.probs)
+	def entropy(self):
+		return sum(map(lambda c: self[c]*surprise(self[c]),self))
+	def surprise(self,evidence):
+		return sum(map(lambda e: surprise(self[e]),evidence))
+	def probOfList(self,evidence):
+		result = 1
+		for e in evidence: result *= self[e]
+		return result
+	def indexOfCoincidence(self):
+		return sum([p**2 for p in self.probs.values()])
+	def sample(self,length=1):
+		def getOne():
+			index = random.uniform(0,1)
+			accumulator = 0
+			for value in sorted(self.probs):
+				accumulator += self.probs[value]
+				if accumulator >= index: return value
+		result = []
+		for i in range(length): result.append(getOne())
+		return result
+	def mean(self):
+		return sum([k*self.probs[k] for k in self.probs])
+	def variance(self):
+		mean = self.mean()
+		return sum([self.probs[k]*(k-mean)**2 for k in self.probs]) 
+	def stdev(self):
+		return self.variance()**0.5
+
+allCharacters = [chr(i) for i in range(256)]
+uniform = lambda vlist: Distribution({v:1/len(vlist) for v in vlist})
+
+RandomLowercase = uniform(string.lowercase)
+RandomCharacters = uniform(allCharacters)
+RandomPrintables = uniform(string.printable)
+myDistribution = uniform(string.hexdigits+"\x00")
+
+English1Grams = Distribution({
+	'e':0.13000,
+	't':0.09056,
+	'a':0.08167,
+	'o':0.07507,
+	'i':0.06966,
+	'n':0.06749,
+	's':0.06327,
+	'h':0.06094,
+	'r':0.05987,
+	'd':0.04253,
+	'l':0.04025,
+	'c':0.02782,
+	'u':0.02758,
+	'm':0.02406,
+	'w':0.02360,
+	'f':0.02228,
+	'g':0.02015,
+	'y':0.01974,
+	'p':0.01929,
+	'b':0.01492,
+	'v':0.00978,
+	'k':0.00772,
+	'j':0.00153,
+	'x':0.00150,
+	'q':0.00095,
+	'z':0.00074
+})
+
+English1GramsUpper = Distribution({string.upper(item):prob for item,prob in English1Grams.probs.items()})
+
+def linComb(distWeights):
+	result = {}
+	for dist, factor in distWeights:
+		for item, prob in dist.probs.items():
+			result.setdefault(item,0)
+			result[item] += prob*float(factor)
+	return Distribution(result)
+
+miscChars = "\x00\x0d\x0a\x20"
+
+softDist = linComb((
+	(English1Grams,0.25),
+	(English1GramsUpper,0.25),
+	(uniform(string.digits),0.2),
+	(uniform(string.punctuation),0.2),
+	(uniform(miscChars),0.1)
+))
+
+
+product = lambda iterable: reduce(operator.mul, iterable, 1)
+
+def hexToBase64(string):
+	return list("".join(string).decode("hex").encode("base64"))[:-1]
+
+def base64Tohex(string):
+	return list("".join(string).decode("base64").encode("hex"))
+
+def project(dicts,vector):
+	return [dicts[i][vector[i]] for i in range(len(vector))]
+
+def collect(tuples):
+	result = {}
+	for t in tuples:
+		if t[0] not in result: result[t[0]] = 0
+		result[t[0]] += t[1]
+	return result
+
+def chrxor(chr1, chr2):
+	return chr(ord(chr1)^ord(chr2))
+
+def strxor(str1, str2):
+	rlength = min(len(str1),len(str2))
+	return [chrxor(str1[i],str2[i]) for i in range(rlength)]
+
+def repKeyXor(string,key):
+	return [chrxor(c,key[i%len(key)]) for i,c in enumerate(string)] 
+
+
+def tryXorBreak(ciphertext,distribution=English1Grams):
+	return min([
+	{"Text":repKeyXor(ciphertext,char),"Key":char} 
+	for char in allCharacters],
+	key = lambda result: distribution.surprise(str(result["Text"]).lower())
+) 
+
+def indexOfCoincidence(buf):
+	matches = 0
+	for i in range(len(buf)):
+		for j in range(len(buf)):
+			if buf[i]==buf[j]: matches += 1
+	return matches/(len(buf)**2)
+
+def mutualIndexOfCoincidence(buf1,buf2):
+	matches = 0
+	for c1 in buf1:
+		for c2 in buf2:
+			if c1==c2: matches +=1
+	return matches/(len(buf1)*len(buf2))
+
+def pad(buf, modulus):
+	padlength = (modulus-(len(buf)))%modulus
+	if padlength==0: padlength=modulus #append at least 1 byte
+	padchar = chr(padlength)
+	return buf + padchar*padlength
+
+def rmPad(buf,modulus):
+	if len(buf)%modulus != 0: raise Exception("Wrong plaintext length") 
+	padChar = buf[-1]
+	padLength = ord(padChar)
+	s = buf[-padLength:]
+	if not all(map(lambda x: x==padChar,s)): raise Exception("Invalid padding")
+	return buf[:len(buf)-padLength]
+	
+
+blocks = lambda buf, blocksize: [buf[blocksize*i:(blocksize*i+blocksize)] for i in range(len(buf)//blocksize + (1 if len(buf)%blocksize!=0 else 0))]
+
+def distributionFromFunction(operands, function):
+	getProb = lambda case: product(project(operands,case))
+	resultDict = collect([(function(case), getProb(case)) for case in itertools.product(*operands)])
+	return Distribution(resultDict)
+
+def xorDecryptionTable(dist):
+	result = {}
+	for c in allCharacters:
+		clist = [(c1,chrxor(c1,c)) for c1 in dist if chrxor(c1,c) in dist]
+		if clist == []: continue
+		result[c] = max(clist,key=lambda x: dist[x[0]]*dist[x[1]])
+	return result
+
+	
---
+++ keyReuseFinder.py
@@ -0,0 +1,146 @@
+#! /usr/bin/python
+from __future__ import division
+from math import log
+import crypto
+import random
+import itertools
+import numpy as np
+import sys
+
+our_pale_attempt_to_represent_minus_infinity = -sys.maxint-1 #minimum integer we can represent
+
+def diagonals(boardsize):
+    """Yields a list of sublists of (x,y) coordinates on a 'boardsize'-sized nxn grid.
+    Each sublist corresponds to a diagonal on the bottom-right triangular half of the grid.
+    Diagonals are iterated bottom-up, and appear leftmost-first."""
+    for i in range(boardsize):
+        yield [(j+i,j) for j in range(boardsize-i)]
+
+def evidenceLogOdds(char, dist):
+    """Gives the log-odd-ratio, in bits, of "character originates in given distribution" vs. "character originates in random distribution"""
+    pDist = dist.probOfList([char]) #probability of char in dist
+    if pDist==0: 
+        return our_pale_attempt_to_represent_minus_infinity
+    pRandom = crypto.RandomCharacters.probOfList([char]) #probability of char in random distribution
+    result = log(pDist,2)-log(pRandom,2)
+    return result
+
+def xtTable(buf,threshold):
+    """Given a buffer, this returns an nxn grid where cell (i,j) corresponds to the log-odd-ratio of (buf[i] xor buf[j]) appearing in the xorText distribution vs. in the random character distribution."""
+    table_size = range(len(buf))
+    return [[\
+            evidenceLogOdds(crypto.chrxor(buf[i],buf[j]),xorText) 
+        for i in table_size]\
+    for j in table_size]
+
+def ptVector(buf):
+    """Given a buffer, this returns an n-size vector where the ith entry corresponds to the log-odd-ration of buf[i] appearing in the plainText distribution vs. in the random character distribution."""
+    return [evidenceLogOdds(c,crypto.softDist) for c in buf]
+
+def partition(nums,goal,breakThreshold):
+    """Takes a vector of numbers as input and returns all delimiters of positive number runs that sum up to more than the goal, as long as they are not interrupted by runs of negative numbers that sum up to the breakThreshold"""
+    result = [] #used to keep runs we have seen that qualify
+    goodVibes = 0 #good vibes collected during current run
+    badVibes = 0 #bad vibes collected during current run
+    lastGoodVibes = 0 #most recent offset where we have encountered good vibes
+    anchor = 0 #starting offset of current run
+    for index,num in enumerate(nums):
+        if num>0:
+            badVibes = 0
+            lastGoodVibes = index
+        else:
+            badVibes+= -num
+        if badVibes>=breakThreshold or index==len(nums)-1:
+            #too much bad vibes, or EOF -- end current run
+            if badVibes<breakThreshold and index==len(nums)-1: 
+                #cutoff by EOF edge case
+                lastGoodVibes+=1
+            if goodVibes>=goal:
+                #enough good vibes - run that just finished qualifies
+                result.append([anchor,lastGoodVibes])
+            anchor = index+1
+            goodVibes = 0
+        else:
+            goodVibes += num 
+    return result
+	
+
+def findPtruns(buf):
+    """Given an input buffer, detects areas likely to be plaintext"""
+    return partition(ptVector(buf), threshold, threshold)
+
+def findparallelciphers(buf):
+    """Given an input buffer, finds index tuples that are likely to be offsets of different strings that had been XORed with the same keystream"""
+    matches = []
+    threshold = (2*log(len(buf),2))-1 #bits
+    table = xtTable(buf,threshold)
+    for diagonal in diagonals(len(buf)):
+        if (0,0) in diagonal: continue #trivial diagonal
+        values = map(lambda (i,j): table[i][j], diagonal)
+        foundRuns = partition(values,threshold,threshold)
+        #format our found runs as (offset,length) instead of (start,end)
+        newmatches = [(diagonal[start],end-start) for (start,end) in foundRuns]
+        matches += newmatches
+    #Remove duplicate results (a suffix of a valid match is also a valid match)
+    for ((o1,o2),l) in matches:
+        for i in range(1,l):
+            cand = ((o1+i,o2+i),l-i) 
+            if cand not in matches: 
+                continue
+            matches.remove(cand)
+    return matches
+
+
+def getHeatMap(mystr, outputFile):
+    """Generates evidence 'heat map' of string based on its evidence gain table (see xtTable for details"""
+    import png
+    table = xtTable(mystr, (2*log(len(mystr),2))-1)
+    #collect all evidence values on table, with 'sanity cutoff' of -5, for statistics
+    tvals = [\
+            table[i][j] for (i,j) in itertools.product(range(len(mystr)),repeat=2)\
+    if t[i][j]>-5]
+    evidenceMean = np.average(tvals)
+    evidenceStdev = np.std(tvals)
+    evidenceScore = lambda x : ((x-evidenceMean) / evidenceStdev) #z-score of evidence
+    #Mean and stdev of discret uniform distribution
+    rgbMean = np.average(range(256))
+    rgbStdev = np.std(range(256))
+    def cutoff(x):
+        if x>255: 
+            return 255
+        if x<0: 
+            return 0
+        return x
+    #Function to convert level of evidence to pixel color
+    epixel = lambda x: (
+        cutoff(rgbMean + evidenceScore(x)*rgbStdev),
+        0,
+        cutoff(rgbMean - evidenceScore(x)*rgbStdev)
+    )
+    pixels = [
+        tuple(
+            reduce(
+                lambda x,y: x+y,
+                [epixel(t[i][j]) for j in range(len(mystr))]
+            )
+        )	 
+    for i in range(len(mystr))
+    ]
+    f = open(outputFile, 'wb')
+    w = png.Writer(len(mystr),len(mystr))
+    w.write(f,pixels)
+    f.close()
+
+
+if __name__ == "__main__":
+    import sys
+    with open(sys.argv[1],'r') as fh:
+        mystr = fh.readlines()[0]
+    ptDistribution = crypto.softDist
+    threshold = (2*log(len(mystr),2))-1
+    xorText = crypto.distributionFromFunction(\
+            [ptDistribution]*2, lambda (c1,c2): crypto.chrxor(c1,c2)\
+    )
+    ptxts =  partition(ptVector(mystr), (threshold+1)/2, (threshold+1)/2)
+    print findparallelciphers(mystr)
+    print ptxts
